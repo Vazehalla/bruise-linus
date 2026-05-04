51bb65fed307ce8d61e3b4df5b80d41892c7e61c
lil-yellow-flower <mathias.ogard@gmail.com>
2026-05-04 18:50:22 +0200

    play on hit effect when attack connects

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,9 +1,12 @@
 class_name State_Attack extends State
 
 var attacking : bool = false
+var _hit_effect_play_id : int = 0
 
 @export var attack_sound : AudioStream
 @export_range(1, 20, 0.5) var decelerate_speed : float = 5.0
+@export var hit_effect_duration : float = 0.12
+@export var hit_effect_frames : int = 4
 @onready var animation_attack: AnimationPlayer = \
 		$"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
@@ -12,6 +15,7 @@ var attacking : bool = false
 @onready var walk: State_Walk = $"../Walk"
 @onready var dash: State_Dash = $"../Dash"
 @onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var on_hit_effect: Sprite2D = $"../../OnHitEffectSprite"
 
 
 
@@ -20,6 +24,8 @@ func Enter() -> void:
 	player.UpdateAnimation("attack")
 	animation_attack.play( "attack_" + player.AnimDirection() )
 	animation_player.animation_finished.connect( EndAttack )
+	if not hurt_box.area_entered.is_connected(_on_hurt_box_area_entered):
+		hurt_box.area_entered.connect(_on_hurt_box_area_entered)
 	audio.stream = attack_sound
 	audio.pitch_scale = randf_range( 0.9, 1.1 )
 	audio.play()
@@ -30,6 +36,8 @@ func Enter() -> void:
 
 func Exit() -> void:
 	animation_player.animation_finished.disconnect( EndAttack )
+	if hurt_box.area_entered.is_connected(_on_hurt_box_area_entered):
+		hurt_box.area_entered.disconnect(_on_hurt_box_area_entered)
 	attacking = false
 	hurt_box.monitoring = false
 
@@ -52,3 +60,27 @@ func HandleInput( _event : InputEvent ) -> State:
 
 func EndAttack( _newAnimName : String ) -> void:
 	attacking = false
+
+
+func _on_hurt_box_area_entered(a: Area2D) -> void:
+	if a is HitBox:
+		_play_hit_effect(a.global_position)
+
+
+func _play_hit_effect(hit_position: Vector2) -> void:
+	_hit_effect_play_id += 1
+	var play_id := _hit_effect_play_id
+	var frame_count := maxi(hit_effect_frames, 1)
+	var step := hit_effect_duration / float(frame_count)
+	on_hit_effect.global_position = hit_position
+	on_hit_effect.frame = 0
+	on_hit_effect.visible = true
+
+	for i in range(frame_count):
+		if play_id != _hit_effect_play_id:
+			return
+		on_hit_effect.frame = i
+		await get_tree().create_timer(step).timeout
+
+	if play_id == _hit_effect_play_id:
+		on_hit_effect.visible = false
