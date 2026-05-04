d1adc5b114f9272fb13ee169f86556c3a46d09f5
lil-yellow-flower <mathias.ogard@gmail.com>
2026-05-04 16:06:37 +0200

    zoom out base and colossus camera by 25 percent

--- Player/Scripts/camera_controller.gd
+++ Player/Scripts/camera_controller.gd
@@ -7,8 +7,8 @@ class_name CameraController extends Camera2D
 @export var deadzone_height : float = 10.0
 @export var look_ahead_distance : float = 14.0
 @export var look_ahead_recover_speed : float = 10.0
-@export var base_zoom : Vector2 = Vector2(0.45, 0.405)
-@export var colossus_zoom : Vector2 = Vector2(0.32, 0.288)
+@export var base_zoom : Vector2 = Vector2(0.34, 0.306)
+@export var colossus_zoom : Vector2 = Vector2(0.24, 0.216)
 @export var zoom_transition_speed : float = 3.5
 @export var shake_default_amplitude : float = 2.0
 @export var shake_default_duration : float = 0.10
