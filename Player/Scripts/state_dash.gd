4b3ce65ee371a6cf963c748dbf3039d058cdb35a
lil-yellow-flower <mathias.ogard@gmail.com>
2026-05-04 17:58:11 +0200

    adjust dash effect y anchor for side dashes

--- Player/Scripts/state_dash.gd
+++ Player/Scripts/state_dash.gd
@@ -8,6 +8,7 @@ class_name State_Dash extends State
 @export var dash_tilt_degrees : float = 28.0
 @export var effect_offset : float = 18.0
 @export var effect_leg_anchor_y : float = 10.0
+@export var effect_side_y_adjust : float = -8.0
 
 var _timer : float = 0.0
 var _cooldown_until : float = 0.0
@@ -18,6 +19,10 @@ var _flip : float = 1.0
 @onready var idle : State_Idle = $"../Idle"
 @onready var walk : State_Walk = $"../Walk"
 @onready var dash_effect : Sprite2D = $"../../DashEffectSprite"
+
+func _effect_anchor_y() -> float:
+	var is_side : bool = absf(_dash_direction.x) > absf(_dash_direction.y)
+	return effect_leg_anchor_y + (effect_side_y_adjust if is_side else 0.0)
 
 func Enter() -> void:
 	player.dash_started.emit()
@@ -30,7 +35,7 @@ func Enter() -> void:
 		_back_direction = Vector2.DOWN
 	_timer = 0.0
 	_flip = player.FacingSign()
-	dash_effect.position = Vector2(0.0, effect_leg_anchor_y)
+	dash_effect.position = Vector2(0.0, _effect_anchor_y())
 	dash_effect.rotation = _back_direction.angle()
 	dash_effect.visible = true
 	player.sprite.scale = Vector2(_flip * dash_scale_x, dash_scale_y)
@@ -41,7 +46,7 @@ func Exit() -> void:
 	player.dash_ended.emit()
 	player.is_invincible = false
 	dash_effect.visible = false
-	dash_effect.position = Vector2(0.0, effect_leg_anchor_y)
+	dash_effect.position = Vector2(0.0, _effect_anchor_y())
 	dash_effect.rotation = 0.0
 	player.sprite.scale = Vector2(_flip, 1.0)
 	player.sprite.rotation = 0.0
@@ -54,7 +59,7 @@ func Process( _delta : float ) -> State:
 	player.sprite.scale.y = lerp(dash_scale_y, 1.0, progress)
 	player.sprite.rotation = _dash_direction.x * deg_to_rad(dash_tilt_degrees * (1.0 - progress))
 	player.velocity = _dash_direction * dash_speed
-	var leg_anchor := Vector2(0.0, effect_leg_anchor_y)
+	var leg_anchor := Vector2(0.0, _effect_anchor_y())
 	dash_effect.position = leg_anchor + (_back_direction * effect_offset * progress)
 	dash_effect.frame = mini(int(progress * 4), 3)
 	if _timer >= dash_duration:
